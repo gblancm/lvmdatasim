8e5f19cad51e7e7a1bf71194beb1b1e1470ec441
Eric Pellegrini <eric.pellegrini@uni-heidelberg.de>
2018-10-01 13:33:36 -0300

    defined internal variables in docstring

--- IFU.py
+++ IFU.py
@@ -1,9 +1,24 @@
 class IFU(object):
     """Read an existing IFU model stored in the data directory as a pickle
-    'ID'= lens id
-    'x'= x position in focal plane
-    'y'= y position in focal plane
-    'r'= lens radius defined as ???
+    Internal Parameters
+    ----------
+    ID: array (init=None)
+        array of lens IDs in the focal plane
+    blockID: array (init=None)
+        IFU block number 
+    blockN: array (init=None)
+        Fiber ID in the block
+    x: array
+        x-position in focal plane
+    y: array
+        y-position in focal plane
+    r: array
+        lens radius defined as ???
+    lensKernel: 2d-array (init to None)
+        Place holder to store a 2d-array lens kernel 
+        calculated from the analytic hexagon and 
+        the datacube PIXSCALE
+
     """
     def __init__ (self, ifuname):
         """ Initialize the IFU class"""
@@ -11,5 +26,6 @@ class IFU(object):
         self.lensx = None
         self.lensy = None
         self.lensr = None
-        self.lensID= None
+        self.lensID = None
         self.blockID= None
+        self.blockN = None
